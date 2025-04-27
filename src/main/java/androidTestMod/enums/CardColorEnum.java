4dec1befb44640cd7e07b123437caf3d22712d83
蝴蝶是幼虫 <2674610176@qq.com>
2025-04-27 17:01:48 +0800

    修bug

--- src/main/java/androidTestMod/enums/CardColorEnum.java
+++ src/main/java/androidTestMod/enums/CardColorEnum.java
@@ -5,7 +5,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 
 public class CardColorEnum {
-    public static final AbstractCard.CardColor YELLOW = AbstractCard.CardColor.add(AndroidTestMod.makeId("YELLOW"));
+    public static final AbstractCard.CardColor YELLOW = AbstractCard.CardColor.add("YELLOW");
     public static final AbstractPlayer.PlayerClass Cangjie = AbstractPlayer.PlayerClass.add(AndroidTestMod.makeId("Cangjie"));
 
 }
