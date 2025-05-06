2392d68740ea321e99c5482ef48469c750ad1429
蝴蝶是幼虫 <2674610176@qq.com>
2025-05-06 14:40:55 +0800

    完成！

--- src/main/java/androidTestMod/enums/CardColorEnum.java
+++ src/main/java/androidTestMod/enums/CardColorEnum.java
@@ -6,6 +6,6 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 
 public class CardColorEnum {
     public static final AbstractCard.CardColor YELLOW = AbstractCard.CardColor.add("YELLOW");
-    public static final AbstractPlayer.PlayerClass Cangjie = AbstractPlayer.PlayerClass.add(AndroidTestMod.makeId("Cangjie"));
+    public static final AbstractPlayer.PlayerClass Cangjie = AbstractPlayer.PlayerClass.add("Cangjie");
 
 }
